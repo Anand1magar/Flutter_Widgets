9d82b17ba258d0822c9d9a14c397fbc8576bcf8a
Anand1magar <anandmagar321@gmail.com>
2021-08-13 19:13:20 +0545

    first commit

---
+++ staggered_grid_view/lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:staggered_grid_view/image_data.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: InstagramSearchGrid(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Staggered Grid View"),
+      ),
+      body: StaggeredGridView.countBuilder(
+        crossAxisCount: 3, //number of item in row
+        itemCount: imageList.length,
+        itemBuilder: (context, index) => ImageCard(
+          imageData: imageList[index],
+        ),
+        staggeredTileBuilder: (index) => StaggeredTile.count(1, 1),
+        mainAxisSpacing: 8.0, // Sepace between items in vertical
+        crossAxisSpacing: 8.0, // Space between item horizentally
+      ),
+    );
+  }
+}
+
+//----------------------Instagram ----------------------
+class InstagramSearchGrid extends StatelessWidget {
+  const InstagramSearchGrid({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StaggeredGridView.countBuilder(
+      crossAxisCount: 3,
+      itemCount: imageList.length,
+      itemBuilder: (context, index) => ImageCard(
+        imageData: imageList[index],
+      ),
+      staggeredTileBuilder: (index) => StaggeredTile.count(
+          (index % 7 == 0) ? 2 : 1, (index % 7 == 0) ? 2 : 1),
+      mainAxisSpacing: 8.0,
+      crossAxisSpacing: 8.0,
+    );
+  }
+}
+
+// ------------------ Pinterest Grid ----------------------
+
+class PinterestGrid extends StatelessWidget {
+  const PinterestGrid({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StaggeredGridView.countBuilder(
+      crossAxisCount: 2,
+      itemCount: imageList.length,
+      itemBuilder: (context, index) => ImageCard(
+        imageData: imageList[index],
+      ),
+      staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+      mainAxisSpacing: 8.0,
+      crossAxisSpacing: 8.0,
+    );
+  }
+}
+
+class ImageCard extends StatelessWidget {
+  ImageData imageData;
+
+  ImageCard({Key key, this.imageData}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16.0),
+      child: Image.network(imageData.imageUrl, fit: BoxFit.cover),
+    );
+  }
+}
